d23ace948c339278b77075bb9414706dbfc540b1
root <root@puppet.openstack.tsm>
2016-03-21 14:54:46 +0000

    delete modules/vswitch

--- profiles/p_libvirt/manifests/cluster.pp
+++ profiles/p_libvirt/manifests/cluster.pp
@@ -1,9 +1,14 @@
 class p_libvirt::cluster (
+  $cluster_name='libvirt',
+  $port=5405,
   $cluster_netname,
-  $multicast_address='239.1.1.2'
+  $multicast_address='239.1.1.2',
+  $votequorum_expected_votes=2
 ){
   # FIXME
-  include p_libvirt
+  class {"p_libvirt":
+    cluster_name => $cluster_name
+  }
 
   $publicif=$netname_to_interface[$cluster_netname]
 
@@ -12,10 +17,20 @@ class p_libvirt::cluster (
     authkey           => '/var/lib/puppet/ssl/certs/ca.pem',
     bind_address      => getvar("ipaddress_$publicif"),
     multicast_address => $multicast_address,
+    votequorum_expected_votes => $votequorum_expected_votes,
+    port => $port,
   }
 
   corosync::service { 'pacemaker':
     version => '0',
   }
 
+  @@firewall{"100 allow p_libvirt::cluster from $fqdn corosync":
+    source => getvar("ipaddress_$publicif"),
+    proto => "udp",
+    dport => $port,
+    action => accept,
+    tag => ["p_libvirt::cluster","$cluster_name"],
+  }
+  Firewall<<| tag=="p_libvirt::cluster" and tag=="$cluster_name" |>>
 }
--- profiles/p_libvirt/manifests/init.pp
+++ profiles/p_libvirt/manifests/init.pp
@@ -1,7 +1,6 @@
 class p_libvirt (
   $cluster_name="libvirt",
-  $vms={},
-  $vm_autostart=true
+  $vms={}
 ) {
   ensure_packages("libvirt-bin")
 
@@ -9,5 +8,29 @@ class p_libvirt (
 
   p_ceph::client {$cluster_name:}
 
-  create_resources(p_libvirt::vmxml,$vms,{autostart => $vm_autostart})
+  create_resources(p_libvirt::vm,$vms)
+
+  file {"/usr/local/bin/create-ceph-secret.sh":
+    ensure => present,
+    mode => "755",
+    source => "puppet:///modules/p_libvirt/create-ceph-secret.sh"
+  } ->
+  exec {"create ceph secret":
+    path => "/usr/bin:/bin",
+    unless => "sh -c \"virsh secret-list | grep \\\"client.$cluster_name\\\"\"",
+    command => "/usr/local/bin/create-ceph-secret.sh $cluster_name $::p_ceph::clients[$name]['key']",
+  }
+
+  #file create xml
+  include vswitch::ovs
+
+  vs_bridge { 'br-ex':
+    ensure => present,
+  }
+
+  vs_port { 'bond0':
+    ensure => present,
+    bridge => 'br-ex',
+  }
+
 }
